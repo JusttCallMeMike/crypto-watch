5a467b4c5bc57b55bb44c2fc57c231fde4b0a05a
Michał Lipiński <michal.lipinski@gmail.com>
2022-09-07 00:00:18 +0200

    Initial commit

---
+++ apps/welcome-page/jest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default {
+  displayName: 'welcome-page',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/welcome-page',
+};
