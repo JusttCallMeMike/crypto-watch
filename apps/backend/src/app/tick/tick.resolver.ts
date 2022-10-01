1ddbf98ab77be9dcf0600eaec4c45beb447efea9
Michał Lipiński <michal.l@justt.ai>
2022-10-01 23:52:16 +0200

    fix resolving pair for tick

--- apps/backend/src/app/tick/tick.resolver.ts
+++ apps/backend/src/app/tick/tick.resolver.ts
@@ -4,15 +4,15 @@ import {
   ParsePairPipe,
   Tick,
   TickService,
-} from '@crypto-watch/db';
-import { Logger, ParseIntPipe } from '@nestjs/common';
-import { Args, Parent, Query, ResolveField, Resolver } from '@nestjs/graphql';
-import { PubSub } from 'graphql-subscriptions';
-import { ObjectId } from 'mongoose';
-import { take } from 'rxjs';
-import { TickArgs } from './dto/tick.arguments';
+} from '@crypto-watch/db'
+import { Logger, ParseIntPipe } from '@nestjs/common'
+import { Args, Parent, Query, ResolveField, Resolver } from '@nestjs/graphql'
+import { PubSub } from 'graphql-subscriptions'
+import { ObjectId } from 'mongoose'
+import { take } from 'rxjs'
+import { TickArgs } from './dto/tick.arguments'
 
-const pubsub = new PubSub();
+const pubsub = new PubSub()
 
 @Resolver((of) => Tick)
 export class TickResolver {
@@ -20,7 +20,7 @@ export class TickResolver {
     private readonly tickService: TickService,
     private readonly pairService: PairService
   ) {}
-  private readonly logger = new Logger(TickResolver.name);
+  private readonly logger = new Logger(TickResolver.name)
 
   //   @Query((returns) => [Tick])
   //   async ticks(@Args('pair', ParsePairPipe) pair: Pair): Promise<Tick[]> {
@@ -31,15 +31,15 @@ export class TickResolver {
   @Query((returns) => [Tick])
   async ticks(@Args() args: TickArgs): Promise<Tick[]> {
     // this.logger.debug(args.pair);
-    const pair = await this.pairService.getByName(args.pair.name);
-    const results = await this.tickService.findAll(pair, args.take);
+    const pair = await this.pairService.getByName(args.pair.name)
+    const results = await this.tickService.findAll(pair, args.take)
     // this.logger.debug(results);
-    return results;
+    return results
   }
-  // FIXME : shouldnt work like this!
+
   @ResolveField('pair')
-  async pair(@Parent() tick: any) {
-    this.logger.debug({ tick });
-    return this.pairService.getOne({ _id: tick.pair });
+  async pair(@Parent() { pair }: { pair: ObjectId }) {
+    this.logger.debug({ pair })
+    return this.pairService.getOne({ _id: pair })
   }
 }
